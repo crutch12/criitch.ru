ed5009af6c04928cd0c3df6d1b5de4ad40e53fc5
Konstantin Barabanov <criitch@yandex.ru>
2024-04-20 11:43:29 +0000

    chore: renew-with-new-domains

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -2,7 +2,7 @@
 # generate-certificate.sh
 
 # Cloudflare Only
-certbot certonly --dns-cloudflare --dns-cloudflare-credentials ./cloudflare.ini -d $DOMAIN_URL --email $DOMAIN_EMAIL --preferred-challenges dns-01 --non-interactive -v --agree-tos
+certbot certonly --renew-with-new-domains --dns-cloudflare --dns-cloudflare-credentials ./cloudflare.ini -d $DOMAIN_URL --email $DOMAIN_EMAIL --preferred-challenges dns-01 --non-interactive -v --agree-tos
 
 # копируем сертификаты из образа certbot в папку Nginx
 cp /etc/letsencrypt/live/*/fullchain.pem /etc/nginx/cert.pem
