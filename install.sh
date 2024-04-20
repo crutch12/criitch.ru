3cc4d37222eb4fc4331614be96c6a32306ee0b56
Konstantin Barabanov <criitch@yandex.ru>
2024-04-20 12:01:36 +0000

    chore: install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
