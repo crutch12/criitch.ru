e877f2f6ef9f3d3b883ff6b793c0306b799a8056
Konstantin Barabanov <criitch@yandex.ru>
2024-04-24 15:00:24 +0000

    feat: bunl (temp); wireguard

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -4,15 +4,22 @@
 # Cloudflare Only
 certbot certonly --renew-with-new-domains --dns-cloudflare --dns-cloudflare-credentials ./cloudflare.ini -d $DOMAIN_URL --email $DOMAIN_EMAIL --preferred-challenges dns-01 --non-interactive -v --agree-tos
 
-# копируем сертификаты из образа certbot в папку Nginx
+# # копируем сертификаты из образа certbot в папку Nginx
 cp /etc/letsencrypt/live/*/fullchain.pem /etc/nginx/root-cert/cert.pem
 cp /etc/letsencrypt/live/*/privkey.pem /etc/nginx/root-cert/key.pem
 
-# копипастим следующий абзац столько раз, сколько * доменов нам нужно (меняем -d значение)
+# # копипастим следующий абзац столько раз, сколько * доменов нам нужно (меняем -d значение)
 
-# ждём, пока dns восстановится после предыдущей генерации
+# # ждём, пока dns восстановится после предыдущей генерации
 echo "wait for 60 seconds"
 sleep 60
 certbot certonly --renew-with-new-domains --dns-cloudflare --dns-cloudflare-credentials ./cloudflare.ini -d $SUB_DOMAIN_URL --email $DOMAIN_EMAIL --preferred-challenges dns-01 --non-interactive -v --agree-tos
 cp /etc/letsencrypt/live/*/fullchain.pem /etc/nginx/sub-cert/cert.pem
 cp /etc/letsencrypt/live/*/privkey.pem /etc/nginx/sub-cert/key.pem
+
+# # ждём, пока dns восстановится после предыдущей генерации
+echo "wait for 60 seconds"
+sleep 60
+certbot certonly --renew-with-new-domains --dns-cloudflare --dns-cloudflare-credentials ./cloudflare.ini -d bunl.criitch.ru -d *.bunl.criitch.ru --email $DOMAIN_EMAIL --preferred-challenges dns-01 --non-interactive -v --agree-tos
+cp /etc/letsencrypt/live/*/fullchain.pem /etc/nginx/bunl-cert/cert.pem
+cp /etc/letsencrypt/live/*/privkey.pem /etc/nginx/bunl-cert/key.pem
